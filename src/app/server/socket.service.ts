2dcbb6325d92ba2c7bab0ae9a6ec257a15f2d27e
tas1337 <tas1337@gmail.com>
2023-09-10 20:24:46 -0400

    s

--- src/app/server/socket.service.ts
+++ src/app/server/socket.service.ts
@@ -13,7 +13,7 @@ export class SocketService {
   }
 
   private initSocket(): void {
-    this.socket = io('http://localhost:3000/');
+    this.socket = io('/');
     this.socket.on('connect', () => {
       this.id = this.socket.id; 
       console.log('connected', this.id);
